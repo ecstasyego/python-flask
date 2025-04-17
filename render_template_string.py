d0ce57a5675a90abe127c4b3562a7ff4d897b758
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2025-04-17 09:48:17 +0900

    Update render_template_string.py

--- render_template_string.py
+++ render_template_string.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template_string
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    html = """
+    <!DOCTYPE html>
+    <html>
+    <head><title>TITLE</title></head>
+    <body>
+        <h1>Hello, World!</h1>
+    </body>
+    </html>
+    """
+    return render_template_string(html)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
